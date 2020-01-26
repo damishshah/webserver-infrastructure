2c083283683ad5541ad1953dc9d184e834d674ca
Damish <damish.shah@gmail.com>
2020-01-26 09:40:27 -0800

    Update init script to stop running after starting the local webserver for cert acquisition

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose"
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-$COMPOSE up -d
+$COMPOSE up -d webserver
 line="0 0 */3 * * $SCRIPTPATH/scripts/ssl_renew.sh"
 (crontab -u ec2-user -l; echo "$line" ) | crontab -u ec2-user -
